702034cb5de08d5162401738c2844cfd95fbb505
Bezawada Lakshith venkat sai <bezawadalakshith5@gmail.com>
2026-01-19 18:32:15 +0530

    Add Bussi component with profile information

---
+++ bussi.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function Bussi() {
+  return (
+    <div className="container">
+      <div className="card">
+        <img src="./logo194.jpeg" alt="profile" className="photo" />
+
+        <h2 className="name">Bezawada Lakshith venkat sai</h2>
+        <p className="role">Aspiring Entrepreneur | Web developer </p>
+
+        <p className="email">bezawadalakshith5@gmail.com</p>
+        <p className="phone">+91 99498 95559</p>
+
+        <p className="skills">React • CSS • JavaScript</p>
+
+        <div className="links">
+          <a
+  className="linkedin"
+  href="https://www.linkedin.com/in/bezawada-lakshith-a331131b5/"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  LinkedIn
+</a>
+
+<a
+  className="github"
+  href="https://github.com/Lakshith07"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  GitHub
+</a>
+
+        </div>
+      </div>
+    </div>
+  );
+}
